d6991ff37f91ef7bf25fe4dffcc734db62b0287a
Louis-Frédéric FORTIER <loufi@localhost.localdomain>
2019-12-02 22:44:18 +0100

    [+](MainPage) Create mainpage navigation bar

--- uikit/lib/main.dart
+++ uikit/lib/main.dart
@@ -10,12 +10,14 @@ class MyApp extends StatelessWidget {
       title: 'UiKit',
       home: Scaffold(
         backgroundColor: Colors.blueGrey[900],
-        body: SafeArea(
-          child: Container(
-            child: Center(
-              child: UikitCard(
-                width: 100,
-                height: 100,
+        bottomNavigationBar: Padding(
+          padding: EdgeInsets.all(20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              UikitCard(
+                width: 75,
+                height: 75,
                 borderRadius: 20.0,
                 child: Icon(
                   Icons.favorite,
@@ -23,10 +25,40 @@ class MyApp extends StatelessWidget {
                   size: 34.0,
                 ),
               ),
-            )
+              UikitCard(
+                width: 75,
+                height: 75,
+                borderRadius: 20.0,
+                child: Icon(
+                  Icons.airplanemode_active,
+                  color: Colors.blue[600],
+                  size: 34.0,
+                ),
+              ),
+              UikitCard(
+                width: 75,
+                height: 75,
+                borderRadius: 20.0,
+                child: Icon(
+                  Icons.alarm,
+                  color: Colors.grey[900],
+                  size: 34.0,
+                ),
+              ),
+              UikitCard(
+                width: 75,
+                height: 75,
+                borderRadius: 20.0,
+                child: Icon(
+                  Icons.battery_full,
+                  color: Colors.green[500],
+                  size: 34.0,
+                ),
+              )
+            ],
           ),
         )
-      ),
+        ),
     );
   }
 }
