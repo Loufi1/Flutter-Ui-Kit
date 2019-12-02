929505c5eca362c48c36d5c010945c2fcc4f06d5
Louis-Frédéric FORTIER <loufi@localhost.localdomain>
2019-12-02 21:03:53 +0100

    [+](Init) Create flutter app

---
+++ uikit/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'UiKit',
+      home: Scaffold(
+        body: Container(
+          color: Colors.blueGrey[50],
+        ),
+      ),
+    );
+  }
+}
